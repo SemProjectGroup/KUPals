bb95ef9b46e38c5629b8535caf7acc1a01f6305c
TchuTcu <praful2062@gmail.com>
2025-07-27 21:46:44 +0545

    used withsharp next config with next sharp optional module

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withSharp({
   images: {
     remotePatterns: [new URL("https://lh3.googleusercontent.com/**")],
   },
-};
+});
 
 export default nextConfig;
