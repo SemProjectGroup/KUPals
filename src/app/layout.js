9315ec44f408250f83e8b8bf95768dbf33e49c8c
TchuTcu <praful2062@gmail.com>
2025-08-03 17:02:00 +0545

    ade some changes to layout js and about us page, also went back tailwind 4 and updated global css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,17 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../context/authContext";
 import Navbar from "../components/layout/navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -10,7 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <AuthProvider>
           <Navbar />
           {children}
